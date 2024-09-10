1d8de3a96b004e754dee86275215e665e1124d11
Alperen Nakiboğlu <96079122+alpnak@users.noreply.github.com>
2024-09-10 17:18:15 +0300

    source files

---
+++ Sources/main.cpp
@@ -0,0 +1,18 @@
+#define SDL_MAIN_HANDLED
+
+#include "mainwindow.h"
+
+#include <QApplication>
+
+#undef main
+
+int main(int argc, char *argv[])
+{    
+    QApplication a(argc, argv);
+
+    MainWindow w;
+
+    w.show();
+
+    return a.exec();
+}
