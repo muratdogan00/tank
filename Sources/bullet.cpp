35a3a846448da2bbae3e9122d9977b6832fee376
muratdogan00 <107349177+muratdogan00@users.noreply.github.com>
2024-09-12 10:46:13 +0300

    Add files via upload

--- Sources/bullet.cpp
+++ Sources/bullet.cpp
@@ -1,18 +1,29 @@
 #include "bullet.h"
 
-Bullet::Bullet(qreal x, qreal y, qreal angle) : QGraphicsEllipseItem(0, 0, 10, 10)
+#include <QBrush>
+
+Bullet::Bullet(qreal x, qreal y, qreal angle) : QGraphicsPixmapItem()
 {
     setPos(x, y);
 
     angle_rad = qDegreesToRadians(angle);
 
-    bullet_speed = 30;
+    bullet_speed = 15;
+    QPixmap bulletpixmap(":/images/bullet.png");
 
-    setBrush(Qt::black);
+    bulletpixmap = bulletpixmap.scaled(6, 30, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    this->setPixmap(bulletpixmap);
+    //this->setFixedSize(bulletpixmap.size());
+
+    //setBrush(Qt::black);
 }
 
 void Bullet::advance()
 {
+    QPointF center = QPointF((bulletpixmap.width()) / 2.0, bulletpixmap.height() / 2.0);
+    this->setTransformOriginPoint(center);
+    qreal angle=qRadiansToDegrees(angle_rad);
+    this->setRotation(angle);
     setPos(x() + qSin(angle_rad) * bullet_speed, y() - qCos(angle_rad) * bullet_speed);
 
     if (pos().x() > 5000 || pos().x() < 0 || pos().y() > 5000 || pos().y() < 0)
